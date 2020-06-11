f8ac5c65cdf927975094e5065857d81c0b060bf6
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-11 14:07:01 -0400

    save

--- database/models/campus.js
+++ database/models/campus.js
@@ -2,20 +2,20 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const Campus = db.define("campus", {
-  firstName: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
+  // firstName: {
+  //   type: Sequelize.STRING,
+  //   allowNull: false,
+  // },
 
-  lastName: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
+  // lastName: {
+  //   type: Sequelize.STRING,
+  //   allowNull: false,
+  // },
 
-  jerseyNumber: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
+  // jerseyNumber: {
+  //   type: Sequelize.INTEGER,
+  //   allowNull: false,
+  // },
 });
 
 module.exports = Campus;
